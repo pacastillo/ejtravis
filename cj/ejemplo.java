1cbc3a4bae9414ed32939a3822193d871a8d4cde
Pedro A. Castillo <pedro@geneura.ugr.es>
2015-11-11 11:58:10 +0100

    añado el código java

---
+++ cj/ejemplo.java
@@ -0,0 +1,19 @@
+/*
+
+javac ejemplo.java
+
+java ejemplo
+
+*/
+
+import java.io.*;
+
+public class ejemplo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+
+    }
+
+}
